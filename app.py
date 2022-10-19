1e3ac2b11779c79840ead648fd7718877f9ecc26
Rishabh Jain <rishabh101.99@gmail.com>
2022-10-18 22:42:32 -0500

    final

--- app.py
+++ app.py
@@ -37,6 +37,33 @@ def getSearchResults(keyword):
 
     return data
 
+def getRecs_(stocks_r):
+    stocks = []
+    for i in range(len(stocks_r)):
+        stocks.append(stocks_r[i][0])
+    data = []
+    for i in range(len(stocks)):
+        data += getRecs(stocks[i])
+
+    if len(data) == 0:
+        query = 'SELECT TickerSymbol FROM ((SELECT TickerSymbol, ROUND(100 * (ClosePrice - OpenPrice) /  OpenPrice, 2) AS PercentChange FROM Stocks NATURAL JOIN (SELECT * FROM Prices WHERE Date = "0000-00-00") AS t1 ORDER BY PercentChange DESC LIMIT 10) AS t2)'
+        cursor.execute(query)
+        data = cursor.fetchall()
+    return data
+
+def getRecs(stock):
+    query = 'SELECT TickerSymbol FROM Stocks NATURAL JOIN Company WHERE Industry = ((SELECT Industry FROM Stocks NATURAL JOIN Company WHERE TickerSymbol = "' + stock + '")) AND TickerSymbol != "' + stock + '" ORDER BY EPS DESC LIMIT 10;'
+    cursor.execute(query)
+    data = cursor.fetchall()
+    
+    if len(data) == 0:
+        query = 'SELECT TickerSymbol FROM ((SELECT TickerSymbol, ROUND(100 * (ClosePrice - OpenPrice) /  OpenPrice, 2) AS PercentChange FROM Stocks NATURAL JOIN (SELECT * FROM Prices WHERE Date = "0000-00-00") AS t1 ORDER BY PercentChange DESC LIMIT 10) AS t2)'
+        cursor.execute(query)
+        data = cursor.fetchall()
+
+    return data
+
+
 
 @app.route("/")
 def main():
@@ -108,7 +135,10 @@ def removeWatchlist(username):
         return redirect('/watchlist/' + id)    
     return redirect('/watchlists/' + username)
 
-    
+def getRecs__(listId):
+    cursor.execute('EXEC Procedure @UserWatchlistId = ' + listId)
+    data = cursor.fetchall()
+    return data
 
 @app.route("/watchlist", defaults={'listId':-1})
 @app.route("/watchlist/<listId>")
@@ -133,7 +163,13 @@ def getWatchlist(listId):
     if len(data) == 0:
         list_html = '<div>This watchlist is currently empty</div>'
 
-    return render_template('watchlist.html', stocks=list_html, listId=listId, userName=userName, listName=listName)
+    data = getRecs_(data)
+    recs_html = '<div>Suggested Stocks<br/><div>Ticker Symbol</div>'
+    for i in range(len(data)):
+        recs_html += '<div>' + str(data[i]) + '</div>'
+    recs_html += '</div>'
+
+    return render_template('watchlist.html', stocks=list_html, listId=listId, userName=userName, listName=listName, recs_html=recs_html)
 
 @app.route("/watchlist", defaults={'listId':-1}, methods = ['POST'])
 @app.route("/watchlist/<listId>", methods = ['POST'])
@@ -157,6 +193,12 @@ def getWatchlistSearch(listId):
     if len(data) == 0:
         list_html = '<div>This watchlist is currently empty</div>'
 
+    data = getRecs_(data)
+    recs_html = '<div>Suggested Stocks<br/><div>Ticker Symbol</div>'
+    for i in range(len(data)):
+        recs_html += '<div>' + str(data[i]) + '</div>'
+    recs_html += '</div>'
+
 
     searches = getSearchResults(request.form['search_bar'])
     searches_html = '<form method="post" action="/addStockToList/' + listId +  '">'
@@ -169,7 +211,7 @@ def getWatchlistSearch(listId):
         searches_html = '<div>' + searches + '</div>'
 
     # print(data)
-    return render_template('watchlist.html', stocks=list_html, search_results=searches_html, listId=listId, userName=userName, listName=listName)
+    return render_template('watchlist.html', stocks=list_html, search_results=searches_html, listId=listId, userName=userName, listName=listName, recs_html=recs_html)
 
 @app.route("/rename", defaults={'listId':-1}, methods = ['POST'])
 @app.route("/rename/<listId>", methods = ['POST'])
@@ -204,6 +246,14 @@ def RemoveStockFromList(listId):
     print(ticker)
     cursor.execute('DELETE FROM WatchlistToTicker WHERE WatchlistId = ' + listId + ' AND TickerSymbol = "' + ticker + '";')
     connection.commit()
+
+    cursor.execute('SELECT * FROM Stocks NATURAL JOIN Watchlists NATURAL JOIN WatchlistToTicker WHERE WatchlistId = ' + listId)
+    data = cursor.fetchall()
+    if len(data) == 0:
+        cursor.execute('DELETE FROM Watchlists WHERE WatchlistId = ' + listId)
+        connection.commit()
+        return redirect('/watchlists/' + 'User1')
+
     return redirect('/watchlist/' + listId)
 
 
@@ -262,30 +312,102 @@ def uploadData():
     #         except:
     #             failed += 1
     #             print("error", failed)
-    with open('Data/stockdata3.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        success = 0
-        failed = 0
-        for row in spamreader:
-            raw = (' '.join(row))
-            tokens = raw.split(',')
-            # print(tokens)
+    # with open('Data/stockdata3.csv', newline='') as csvfile:
+    #     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    #     success = 0
+    #     failed = 0
+    #     for row in spamreader:
+    #         raw = (' '.join(row))
+    #         tokens = raw.split(',')
+    #         # print(tokens)
 
-            query = 'INSERT INTO Company VALUES ("' + tokens[0] + '", "' + tokens[1] + '", ' + tokens[2] + ', ' + tokens[3] + '", "' + tokens[4] + '")'
-            # print(query)
-            try:
-                cursor.execute(query)
-                connection.commit()
+    #         query = 'INSERT INTO Company VALUES ("' + tokens[0] + '", "' + tokens[1] + '", ' + tokens[2] + ', ' + tokens[3] + '", "' + tokens[4] + '")'
+    #         # print(query)
+    #         try:
+    #             cursor.execute(query)
+    #             connection.commit()
 
-                success += 1
-                print(success)
-            except:
-                failed += 1
-                print(query)
-                print("error", failed)
-            
+    #             success += 1
+    #             print(success)
+    #         except:
+    #             failed += 1
+    #             print(query)
+    #             print("error", failed)     
 
     return "done"
 
+@app.route("/trigger")
+def addTrigger():
+    query = 'CREATE PROCEDURE Procedure (IN UserWatchlistId INT) BEGIN DECLARE finished int default 0; DECLARE stock VARCHAR(5); DECLARE cur CURSOR FOR (SELECT TickerSymbol FROM WatchlistToTicker NATURAL JOIN Watchlists WHERE WatchlistId = UserWatchlistId GROUP BY WatchlistId); DECLARE CONTINUE HANDLER FOR NOT FOUND SET FINISHED = 1; DROP TABLE IF EXISTS SuggestionsTable; CREATE TABLE SuggestionsTable (TickerSymbol VARCHAR(5)); OPEN cur; REPEAT FETCH cur INTO stock; INSERT INTO SuggestionsTable (TickerSymbol) SELECT TickerSymbol FROM ((SELECT TickerSymbol, ROUND(100 * (ClosePrice - OpenPrice) /  OpenPrice, 2) AS PercentChange FROM Stocks NATURAL JOIN (SELECT * FROM Prices WHERE Date = "0000-00-00") AS t1 ORDER BY PercentChange DESC LIMIT 10) AS t2) UNTIL finished END REPEAT; close cur; IF NOT EXISTS (SELECT * FROM SuggestionsTable) THEN INSERT INTO SuggestionsTable (TickerSymbol) SELECT TickerSymbol FROM ((SELECT TickerSymbol, ROUND(100 * (ClosePrice - OpenPrice) /  OpenPrice, 2) AS PercentChange FROM Stocks NATURAL JOIN (SELECT * FROM Prices WHERE Date = "0000-00-00") AS t1 ORDER BY PercentChange DESC LIMIT 10) AS t2) END IF; SELECT DISTINCT TickerSymbol FROM SuggestionsTable; END'
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("success")
+    except Exception as e:
+        print(query)
+        print("error", e)   
+    return "done"
+
+@app.route("/procedure")
+def addProcedure():
+    # query = 'CREATE PROCEDURE `new_procedure` (IN UserWatchlistId INT) BEGIN DECLARE finished int default 0; DECLARE stock VARCHAR(5); DECLARE cur CURSOR FOR (SELECT TickerSymbol FROM WatchlistToTicker NATURAL JOIN Watchlists WHERE WatchlistId = UserWatchlistId GROUP BY WatchlistId); DECLARE CONTINUE HANDLER FOR NOT FOUND SET FINISHED = 1; DROP TABLE IF EXISTS SuggestionsTable; CREATE TABLE SuggestionsTable (TickerSymbol VARCHAR(5)); OPEN cur; REPEAT FETCH cur INTO stock; INSERT INTO SuggestionsTable (TickerSymbol) SELECT TickerSymbol FROM Stocks NATURAL JOIN Company WHERE Industry = (SELECT Industry FROM Stocks NATURAL JOIN Company WHERE TickerSymbol = stock AS t3) AND TickerSymbol != stock ORDER BY EPS DESC LIMIT 1; UNTIL finished END REPEAT; close cur; SELECT DISTINCT TickerSymbol FROM SuggestionsTable; END'
+    # try:
+    #     cursor.execute(query)
+    #     connection.commit()
+    #     print("success")
+    # except Exception as e:
+    #     print(query)
+    #     print("error", e)   
+    # return "done"
+
+    return str(getRecs('abt'))
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+## Stored Procedure
+
+# CREATE PROCEDURE Procedure @UserWatchlistId INT
+# BEGIN
+# DECLARE finished int default 0;
+# DECLARE stock VARCHAR(5);
+
+# DECLARE cur CURSOR FOR (SELECT TickerSymbol FROM WatchlistToTicker NATURAL JOIN Watchlists WHERE WatchlistId = UserWatchlistId GROUP BY WatchlistId)
+
+
+# DECLARE CONTINUE HANDLER FOR NOT FOUND SET FINISHED = 1;
+
+# DROP TABLE IF EXISTS SuggestionsTable;
+# CREATE TABLE SuggestionsTable (TickerSymbol VARCHAR(5));
+
+# OPEN cur;
+# REPEAT
+# 	FETCH cur INTO stock;
+
+# 	INSERT INTO SuggestionsTable
+# SELECT TickerSymbol FROM Stocks NATURAL JOIN Company WHERE Industry = (SELECT Industry FROM Stocks NATURAL JOIN Company WHERE TickerSymbol = stock) AND TickerSymbol != stock ORDER BY EPS DESC LIMIT 1;
+
+# END REPEAT;
+# close cur;
+
+# IF NOT EXISTS (SELECT * FROM SuggestionsTable) THEN
+# 	INSERT INTO SuggestionsTable
+# SELECT TickerSymbol FROM (SELECT TickerSymbol, ROUND(100 * (ClosePrice - OpenPrice) /  OpenPrice, 2) AS PercentChange FROM Stocks NATURAL JOIN (SELECT * FROM Prices WHERE Date = "0000-00-00") AS t1 ORDER BY PercentChange DESC LIMIT 10);
+# END IF;
+
+# SELECT DISTINCT TickerSymbol
+# FROM SuggestionsTable;
+
+# END
+
+
+
+## Trigger
+
+# CREATE DEFINER=`root`@`localhost` TRIGGER `Flask`.`WatchlistToTicker_AFTER_DELETE` AFTER DELETE ON `WatchlistToTicker` FOR EACH ROW
+# BEGIN
+# 	IF old.WatchlistId NOT IN (SELECT DISTINCT WatchlistId FROM WatchlistToTicker) THEN
+# 		DELETE FROM Watchlists WHERE WatchlistId = old.WatchlistId;
+# 	END IF;
+# END
